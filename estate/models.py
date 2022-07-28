25ef58aa73a0066658e638a81c0da31b600134eb
trau <oscarnguyen126@gmail.com>
2022-07-28 16:52:09 +0700

    Add state field, filter function

--- estate/models.py
+++ estate/models.py
@@ -1,4 +1,6 @@
 from odoo import models, fields
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 
 class Estate(models.Model):
@@ -7,12 +9,16 @@ class Estate(models.Model):
 
     name = fields.Char(string='Title', required=True)
     description = fields.Char()
-    date_availability = fields.Date(copy=False)
-    expected_price = fields.Float(string="Selling price ($)")
+    date_availability = fields.Date(copy=False, default=datetime.now() + relativedelta(months=3))
+    expected_price = fields.Float(string="Expected price ($)")
     selling_price = fields.Float(string="Selling price ($)", readonly=True, copy=False)
     bedrooms = fields.Integer(string="Bedrooms", default=2)
-    living_area = fields.Integer(string="Living square (m2)")
+    living_area = fields.Integer(string="Living square (sqrm)")
     facades = fields.Integer()
     garage = fields.Boolean()
-    garden_area = fields.Integer(string="Garden square (m2)")
+    garden_area = fields.Integer(string="Garden square (sqrm)")
     garden_orientation = fields.Selection([{'north', 'North'}, {'south', 'South'}, {'east', 'East'}, {'west', 'West'}])
+    state = fields.Selection([
+                              {'offer received', 'Offer Received'},
+                              {'offer accepted', 'Offer Accepted'},
+                              {'sold', 'Sold'}, {'canceled', 'Canceled'}], default='New', copy=False)
