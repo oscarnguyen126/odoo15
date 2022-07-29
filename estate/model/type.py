e67f14bb45f719b0ceeb5b5dd81cae6979fdb942
trau <oscarnguyen126@gmail.com>
2022-07-29 11:22:40 +0700

    Create type model

---
+++ estate/model/type.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+
+class Type(models.Model):
+    _name = "estate.property.type"
+    _description = "Property type"
+
+    name = fields.Char(string='Type', required=True)
