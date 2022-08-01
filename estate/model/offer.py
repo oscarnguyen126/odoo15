8128af2db261924c2e7d607798d34246caa59ae5
trau <oscarnguyen126@gmail.com>
2022-08-01 15:24:31 +0700

    add function check price field

--- estate/model/offer.py
+++ estate/model/offer.py
@@ -1,5 +1,4 @@
-from email.policy import default
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Offer(models.Model):
@@ -9,6 +8,16 @@ class Offer(models.Model):
     status = fields.Selection([{'accepted', 'Accepted'}, {'refused', 'Refused'}], copy=False)
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property', required=True)
-    vadility = fields.Integer(default=7)
-    date_deadline = fields.Date()
+    validity = fields.Integer(default=7, string="Validity (days)")
+    # date_deadline = fields.Date(compute="_compute_total", inverse="_inverse_total")
     
+    
+    # @api.depends("validity")
+    # def _compute_total(self):
+    #     for record in self:
+    #         record.date_deadline = record.create_date + record.validity
+            
+    
+    # def _inverse_total(self):
+    #     for record in self:
+    #         record.create_date = record.date_deadline - record.validity
